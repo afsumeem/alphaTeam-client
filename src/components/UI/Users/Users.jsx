268f0e650ba8de055a83c56f2414caa65fa0f372
Afsana Meem <afsumeem@gmail.com>
2023-12-02 21:36:21 +0600

    fetch users from api

--- src/components/UI/Users/Users.jsx
+++ src/components/UI/Users/Users.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import "./Users.css";
+import { Col, Row } from "react-bootstrap";
 
 const Users = () => {
   // fetch users data
@@ -12,7 +14,53 @@ const Users = () => {
   }, []);
   // console.log(userData);
 
-  return <div></div>;
+  return (
+    <div className="mt-5 ">
+      <div className="mb-5 mx-5">
+        <h4
+          className="fw-bold text-uppercase"
+          style={{ color: "blue", fontSize: "25px" }}
+        >
+          Alpha Team
+        </h4>
+        <h2
+          className="fw-semibold text-uppercase mb-5"
+          style={{ fontSize: "17px" }}
+        >
+          Alpha Team members
+        </h2>
+        <hr />
+      </div>
+      <Row>
+        {userData.map((user, index) => (
+          <Col key={index} sm={12} md={6} lg={4}>
+            <div className="userCard">
+              <div className="coverBox">
+                <div className="userCover">
+                  <img src={user.avatar} alt="" />
+                </div>
+              </div>
+              <div className="userDetails">
+                <h2>
+                  {user.first_name} {user.last_name}
+                </h2>
+                <h4>{user.email}</h4>
+              </div>
+              <div className="boxLine"></div>
+              <div className="boxLine"></div>
+
+              <div className="userProfile">
+                <p>Gender: {user.gender}</p>
+                <p>Domain: {user.domain}</p>
+                <br />
+                <p>{user.available}</p>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default Users;
